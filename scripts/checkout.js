f7df6380a0afb0137c8634abe7f08ce0ec8154ab
Zohaib Umer <umerzohaib679@gmail.com>
2026-01-30 10:35:45 +0500

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,7 +20,7 @@ cart.forEach((cartItem) => {
     }
 
     cartSummaryHTML += 
-    `<div class="cart-item-container">
+    `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -107,5 +107,7 @@ document.querySelectorAll('.js-delete-link')
     const productId = link.dataset.productId;
     removeFromCart(productId);
 
+    const deleteProduct = document.querySelector(`.js-cart-item-container-${productId}`)
+    deleteProduct.remove();
     })
 })
