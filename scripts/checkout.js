a6419cb1aec21d05622b1ec801ec58d693163712
Zohaib Umer <umerzohaib679@gmail.com>
2026-02-06 18:39:38 +0500

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
+import '../data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
